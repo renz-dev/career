50d562996a17686d9698f5f0c9b95da24a556872
renz-dev <h1sokuuqqx@gmail.com>
2026-07-08 21:15:20 +0300

    08-07

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-const burger = document.getElementById('burger');
-const menu = document.getElementById('mobileMenu');
+const burger = document.getElementById("burger");
+const menu = document.getElementById("mobileMenu");
 
-burger.addEventListener('click', () => {
-    menu.classList.toggle('active');
+burger.addEventListener("click", () => {
+  menu.classList.toggle("active");
 });
+
+const reviewsSlider = document.querySelector(".reviews__slider");
+new Swiper(reviewsSlider, {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".reviews-next",
+    prevEl: ".reviews-prev",
+  },
+  breakpoints: {
+    683: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    911: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
+
+const slides = document.querySelectorAll('.reviews__item');
+
+
